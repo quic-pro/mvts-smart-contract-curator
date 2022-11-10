6fc914747dcaa499de17d8e7a337599e64d4525c
Kosrotoff <50526865+Kosrotoff@users.noreply.github.com>
2022-11-10 11:18:31 +0700

    Fixed codestyle

--- contracts/Curator.sol
+++ contracts/Curator.sol
@@ -9,28 +9,28 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Curator is Ownable {
     // ----- CUSTOM TYPES ------------------------------------------------------
-    
+
     struct Router {
         uint128 ttl;
         uint128 chainId;
         string adr;
     }
-    
-    
-    
+
+
+
     // ----- DATA --------------------------------------------------------------
-    
+
     Router public rootRouter;
-    
-    
-    
+
+
+
     // ----- SMART CONTRACT MANAGEMENT ------------------------------------------
 
-    function setRootRouter(uint128 ttl, uint128 chainId, string memory adr) external onlyOwner {
+    function setRootRouter(uint128 chainId, string memory adr, uint128 ttl) external onlyOwner {
         rootRouter = Router(ttl, chainId, adr);
     }
-    
+
     function cleanRootRouter() external onlyOwner {
-        rootRouter = Router(0, 0, "");
+        delete rootRouter;
     }
 }
