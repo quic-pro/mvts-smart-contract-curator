6fe078229ba0b865c0f0a0209ca4e3b5bc5530b1
Kosrotoff <50526865+Kosrotoff@users.noreply.github.com>
2022-11-14 23:16:48 +0700

    Updated contract, truffle config and README.md

--- contracts/Curator.sol
+++ contracts/Curator.sol
@@ -1,36 +1,66 @@
 // SPDX-License-Identifier: MIT
-
-
-pragma solidity 0.8.7;
+pragma solidity 0.8.17;
 
 
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 
 
 contract Curator is Ownable {
-    // ----- CUSTOM TYPES ------------------------------------------------------
+    // ----- CUSTOM TYPES ----------------------------------------------------------------------------------------------
 
-    struct Router {
-        uint128 ttl;
-        uint128 chainId;
+    struct RootRouter {
+        string chainId;
         string adr;
+        string ttl;
     }
 
 
 
-    // ----- DATA --------------------------------------------------------------
+    // ----- DATA ------------------------------------------------------------------------------------------------------
 
-    Router public rootRouter;
+    RootRouter public rootRouter;
+    bool public isRootRouterConfigured;
 
 
 
-    // ----- SMART CONTRACT MANAGEMENT ------------------------------------------
+    // ----- SMART CONTRACT MANAGEMENT ---------------------------------------------------------------------------------
 
-    function setRootRouter(uint128 chainId, string memory adr, uint128 ttl) external onlyOwner {
-        rootRouter = Router(ttl, chainId, adr);
+    function setRootRouter(uint256 newChainId, string memory newAddress, uint256 newTtl) external onlyOwner {
+        rootRouter = RootRouter(
+            Strings.toString(newChainId),
+            newAddress,
+            Strings.toString(newTtl)
+        );
+        isRootRouterConfigured = true;
     }
 
     function cleanRootRouter() external onlyOwner {
         delete rootRouter;
+        isRootRouterConfigured = false;
+    }
+
+
+
+    // ----- ROUTING ---------------------------------------------------------------------------------------------------
+
+    function getRootRouter() public view returns(string[5] memory) {
+        if (!isRootRouterConfigured) {
+            return [
+                "400", // Response code
+                "", // Ignore
+                "", // Ignore
+                "", // Ignore
+                "" // Ignore
+            ];
+        }
+
+        return [
+            "200", // Response code
+            "3", // Pool code length
+            rootRouter.chainId,
+            rootRouter.adr,
+            rootRouter.ttl
+        ];
     }
 }
